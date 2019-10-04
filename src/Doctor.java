17d775a9dbe0b84cdf03ffc2eb16e2da4a9e5546
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-10-05 00:01:07 +0200

    add Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,9 +3,16 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Costruiendo el Objeto Doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es:" + name);
+    }
+
     //Comportamientos
     public void showName(){
         System.out.println(name);
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Gian Pumayalla");
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
     }
