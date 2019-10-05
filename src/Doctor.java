0f7e2bb04d757b2e708a9aeafa002fc1150e3d72
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-10-05 13:15:04 +0200

    UI menu

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,18 +1,25 @@
 public class Doctor {
-    int id;
+    //Atributos
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor(){
-        System.out.println("Costruiendo el Objeto Doctor");
+        System.out.println("Construyendo el Objeto Doctor");
+        id++;
     }
 
     Doctor(String name){
-        System.out.println("El nombre del Doctor asignado es:" + name);
+        System.out.println("El nombre del Doctor asignado es: " + name);
     }
 
     //Comportamientos
     public void showName(){
         System.out.println(name);
     }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Gian Pumayalla");
-        myDoctor.name = "Alejandro Rodriguez";
+
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        showMenu();
     }
 }
